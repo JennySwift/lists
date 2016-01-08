e9d26178201869ca0aa78be51e0262a31246b39b
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-08 17:20:24 +1100

    Item popup css

--- resources/views/pages/items/item-popup-component.blade.php
+++ resources/views/pages/items/item-popup-component.blade.php
@@ -41,66 +41,70 @@
                 rows="10">
         </textarea>
 
-            <div class="form-group">
-                <label for="item-popup-category">Category</label>
+            <div class="flex">
+                <div class="form-group">
+                    <label for="item-popup-category">Category</label>
 
-                <select
-                        v-model="selectedItem.category"
-                        id="item-popup-category"
-                        class="form-control"
-                >
-                    <option v-for="category in categories" v-bind:value="category">
-                        @{{ category.name }}
-                    </option>
-                </select>
-            </div>
-            
-            <div class="form-group">
-                <label for="selected-item-priority">Priority</label>
-                <input
-                    v-model="selectedItem.priority"
-                    type="number"
-                    id="selected-item-priority"
-                    name="selected-item-priority"
-                    placeholder="priority"
-                    class="form-control"
-                >
-            </div>
-            
-            <div class="form-group">
-                <label for="selected-item-urgency">Urgency</label>
-                <input
-                    v-model="selectedItem.urgency"
-                    type="number"
-                    id="selected-item-urgency"
-                    name="selected-item-urgency"
-                    placeholder="urgency"
-                    class="form-control"
-                >
+                    <select
+                            v-model="selectedItem.category"
+                            id="item-popup-category"
+                            class="form-control"
+                    >
+                        <option v-for="category in categories" v-bind:value="category">
+                            @{{ category.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="selected-item-alarm">Alarm</label>
+                    <input
+                            v-model="selectedItem.alarm"
+                            type="text"
+                            id="selected-item-alarm"
+                            name="selected-item-alarm"
+                            placeholder="alarm"
+                            class="form-control"
+                    >
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="selected-item-parent">Parent Id</label>
-                <input
-                    v-model="selectedItem.parent_id"
-                    type="text"
-                    id="selected-item-parent"
-                    name="selected-item-parent"
-                    placeholder="parent.id"
-                    class="form-control"
-                >
-            </div>
-            
-            <div class="form-group">
-                <label for="selected-item-alarm">Alarm</label>
-                <input
-                    v-model="selectedItem.alarm"
-                    type="text"
-                    id="selected-item-alarm"
-                    name="selected-item-alarm"
-                    placeholder="alarm"
-                    class="form-control"
-                >
+            <div class="flex">
+                <div class="form-group">
+                    <label for="selected-item-priority">Priority</label>
+                    <input
+                            v-model="selectedItem.priority"
+                            type="number"
+                            id="selected-item-priority"
+                            name="selected-item-priority"
+                            placeholder="priority"
+                            class="form-control"
+                    >
+                </div>
+
+                <div class="form-group">
+                    <label for="selected-item-urgency">Urgency</label>
+                    <input
+                            v-model="selectedItem.urgency"
+                            type="number"
+                            id="selected-item-urgency"
+                            name="selected-item-urgency"
+                            placeholder="urgency"
+                            class="form-control"
+                    >
+                </div>
+
+                <div class="form-group">
+                    <label for="selected-item-parent">Parent Id</label>
+                    <input
+                            v-model="selectedItem.parent_id"
+                            type="text"
+                            id="selected-item-parent"
+                            name="selected-item-parent"
+                            placeholder="parent.id"
+                            class="form-control"
+                    >
+                </div>
             </div>
 
             <div class="buttons">
