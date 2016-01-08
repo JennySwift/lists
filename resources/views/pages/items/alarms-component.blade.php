ace35ff5d772ed08c891c90e2bcd17a4dba93d14
Jenny Swift <cheezyspaghetti@gmail.com>
2016-01-08 19:15:11 +1100

    Get rid of vue fragment instance warnings

--- resources/views/pages/items/alarms-component.blade.php
+++ resources/views/pages/items/alarms-component.blade.php
@@ -1,30 +1,32 @@
 <script id="alarms-template" type="x-template">
 
-    <div v-if="items.length > 0" id="alarms">
-        <h5>Alarms</h5>
+    <div>
+        <div v-if="items.length > 0" id="alarms">
+            <h5>Alarms</h5>
 
-        <div id="alarm"></div>
+            <div id="alarm"></div>
 
-        <item
-                v-for="item in items | orderBy 'priority'"
-                {{--:items-filter="itemsFilter"--}}
-                {{--:title-filter="titleFilter"--}}
-                {{--:priority-filter="priorityFilter"--}}
-                {{--:category-filter="categoryFilter"--}}
-                :show-loading.sync="showLoading"
-                :show-item-popup.sync="showItemPopup"
-                {{--:show-children="showChildren"--}}
-                {{--:items.sync="items"--}}
-                :item="item"
-                :selected-item.sync="selectedItem"
-                {{--:zoomed-item="zoomedItem"--}}
-                {{--:get-items="getItems"--}}
-                :categories="categories"
-                :delete-item="deleteItem"
-                {{--:zoom="zoom"--}}
-                {{--class="item-with-children"--}}
-        >
-        </item>
+            <item
+                    v-for="item in items | orderBy 'priority'"
+                    {{--:items-filter="itemsFilter"--}}
+                    {{--:title-filter="titleFilter"--}}
+                    {{--:priority-filter="priorityFilter"--}}
+                    {{--:category-filter="categoryFilter"--}}
+                    :show-loading.sync="showLoading"
+                    :show-item-popup.sync="showItemPopup"
+                    {{--:show-children="showChildren"--}}
+                    {{--:items.sync="items"--}}
+                    :item="item"
+                    :selected-item.sync="selectedItem"
+                    {{--:zoomed-item="zoomedItem"--}}
+                    {{--:get-items="getItems"--}}
+                    :categories="categories"
+                    :delete-item="deleteItem"
+                    {{--:zoom="zoom"--}}
+                    {{--class="item-with-children"--}}
+            >
+            </item>
+        </div>
     </div>
 
 </script>
