e805dbb43a5fdd86794cf00904faa7dac984f46e
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-18 15:28:46 +1100

    Start to get app better for small screens

--- resources/views/pages/items/item/before-item.blade.php
+++ resources/views/pages/items/item/before-item.blade.php
@@ -15,30 +15,100 @@
         0
     </span>
 
-    <button v-on:click="deleteItem(item)" class="btn-danger btn-xs delete-item">
+    <button
+        v-on:click="deleteItem(item)"
+        class="btn-danger btn-xs delete-item big-screen"
+    >
         <span class="fa fa-times"></span>
     </button>
 
     <i
         v-link="{ path: '/items/:' + item.id }"
-        class="fa fa-search-plus">
+        class="fa fa-search-plus big-screen"
+    >
     </i>
 
     <i
             v-if="item.has_children && (!item.children || item.children.length === 0)"
             v-on:click="getItems('expand', item)"
-            class="fa fa-plus">
+            class="fa fa-plus big-screen"
+    >
     </i>
 
     <i
             v-if="item.has_children && item.children && item.children.length > 0"
             v-on:click="collapseItem(item)"
-            class="fa fa-minus">
+            class="fa fa-minus big-screen"
+    >
     </i>
 
     <i
             v-if="!item.has_children"
-            class="fa fa-plus my-hidden">
+            class="fa fa-plus my-hidden big-screen"
+    >
     </i>
+
+
+
+    {{--Actions dropdown for small screens--}}
+    <div class="btn-group">
+
+        <button
+            type="button"
+            class="btn btn-default btn-xs dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+        >
+            <span class="caret"></span>
+        </button>
+
+        <ul class="dropdown-menu">
+            <li>
+                <a
+                    href="#"
+                    v-on:click="deleteItem(item)"
+                    class="fa fa-times"
+                >
+                </a>
+            </li>
+
+            <li>
+                <a
+                    href="#"
+                    v-link="{ path: '/items/:' + item.id }"
+                    class="fa fa-search-plus"
+                >
+                </a>
+            </li>
+
+            <li v-if="item.has_children && (!item.children || item.children.length === 0)">
+                <a
+                    href="#"
+                    v-on:click="getItems('expand', item)"
+                    class="fa fa-plus"
+                >
+                </a>
+            </li>
+
+            <li  v-if="item.has_children && item.children && item.children.length > 0">
+                <a
+                        href="#"
+                        v-on:click="collapseItem(item)"
+                        class="fa fa-minus"
+                >
+                </a>
+            </li>
+
+            <li v-if="!item.has_children">
+                <a
+                        href="#"
+                        class="fa fa-plus my-hidden"
+                >
+                </a>
+            </li>
+        </ul>
+
+    </div>
 
 </div>
