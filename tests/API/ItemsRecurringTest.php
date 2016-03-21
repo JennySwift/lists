e262daa02fdf52e16aaab4f6ac00d66975caf380
Jenny Swift <cheezyspaghetti@gmail.com>
2016-03-21 15:12:17 +1100

    Write more tests for recurring items feature

--- tests/API/ItemsRecurringTest.php
+++ tests/API/ItemsRecurringTest.php
@@ -99,7 +99,7 @@ class ItemsRecurringTest extends TestCase
         $this->assertEquals(1, $item->recurring_frequency);
         $this->assertEquals(Carbon::tomorrow()->format('Y-m-d H:i:s'), $item->not_before);
 
-        //Make the recurring frequency 5 and the recurring frequency 'month' and the note before date way in the future (so it's fixed and testable rather than dynamic)
+        //Make the recurring frequency 5 and the recurring frequency 'month' and the not before date way in the future (so it's fixed and testable rather than dynamic)
         $response = $this->call('PUT', '/api/items/'.$item->id, [
             'not_before' => '2050-01-10 15:30:00',
             'recurring_unit' => 'month',
@@ -119,8 +119,94 @@ class ItemsRecurringTest extends TestCase
         //dd($content);
 
         $this->checkitemKeysExist($content);
-        
+
         $this->assertEquals('2050-06-10 15:30:00', $content['notBefore']);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        DB::rollBack();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_calculate_the_next_time_for_a_recurring_item_that_has_a_not_before_time_in_the_past_and_a_recurring_frequency_of_2_and_a_recurring_unit_of_years()
+    {
+        DB::beginTransaction();
+        $this->logInUser();
+
+        $item = Item::forCurrentUser()->whereNotNull('recurring_unit')->first();
+
+        $this->assertEquals('minute', $item->recurring_unit);
+        $this->assertEquals(1, $item->recurring_frequency);
+        $this->assertEquals(Carbon::tomorrow()->format('Y-m-d H:i:s'), $item->not_before);
+
+        //Make the recurring frequency 5 and the recurring frequency 'year' and the not before date in the past
+        $response = $this->call('PUT', '/api/items/'.$item->id, [
+            'not_before' => '2016-01-10 15:30:00',
+            'recurring_unit' => 'year',
+            'recurring_frequency' => 2
+        ]);
+        $content = json_decode($response->getContent(), true);
+        //dd($content);
+        $this->assertEquals('2016-01-10 15:30:00', $content['notBefore']);
+        $this->assertEquals('year', $content['recurringUnit']);
+        $this->assertEquals(2, $content['recurringFrequency']);
+
+        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
+        $response = $this->call('PUT', '/api/items/'.$item->id, [
+            'updatingNextTimeForRecurringItem' => true
+        ]);
+        $content = json_decode($response->getContent(), true);
+        //dd($content);
+
+        $this->checkitemKeysExist($content);
+
+        $this->assertEquals('2018-01-10 15:30:00', $content['notBefore']);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        DB::rollBack();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function it_can_calculate_the_next_time_for_a_recurring_item_that_has_a_not_before_time_three_years_ago_and_a_recurring_frequency_of_2_and_a_recurring_unit_of_years()
+    {
+        DB::beginTransaction();
+        $this->logInUser();
+
+        $item = Item::forCurrentUser()->whereNotNull('recurring_unit')->first();
+
+        $this->assertEquals('minute', $item->recurring_unit);
+        $this->assertEquals(1, $item->recurring_frequency);
+        $this->assertEquals(Carbon::tomorrow()->format('Y-m-d H:i:s'), $item->not_before);
+
+        //Make the recurring frequency 5 and the recurring frequency 'year' and the not before date in the past
+        $response = $this->call('PUT', '/api/items/'.$item->id, [
+            'not_before' => '2013-01-10 15:30:00',
+            'recurring_unit' => 'year',
+            'recurring_frequency' => 2
+        ]);
+        $content = json_decode($response->getContent(), true);
+        //dd($content);
+        $this->assertEquals('2013-01-10 15:30:00', $content['notBefore']);
+        $this->assertEquals('year', $content['recurringUnit']);
+        $this->assertEquals(2, $content['recurringFrequency']);
+
+        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
+        $response = $this->call('PUT', '/api/items/'.$item->id, [
+            'updatingNextTimeForRecurringItem' => true
+        ]);
+        $content = json_decode($response->getContent(), true);
+        //dd($content);
+
+        $this->checkitemKeysExist($content);
+
+        $this->assertEquals('2017-01-10 15:30:00', $content['notBefore']);
 
         $this->assertEquals(200, $response->getStatusCode());
 
@@ -179,54 +265,5 @@ class ItemsRecurringTest extends TestCase
         DB::rollBack();
     }
 
-    /**
-     * @test
-     * @return void
-     */
-    public function it_can_calculate_the_next_time_for_a_recurring_item_that_has_a_not_before_time_in_the_past_and_a_recurring_frequency_of_5()
-    {
-        DB::beginTransaction();
-        $this->logInUser();
 
-        $item = Item::forCurrentUser()->whereNotNull('recurring_unit')->first();
-
-        //Check the recurring values are as expected
-        $this->assertEquals('minute', $item->recurring_unit);
-        $this->assertEquals(1, $item->recurring_frequency);
-
-        //Make the not before time in the past
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'not_before' => '2016-03-01 13:30:05'
-        ]);
-        $content = json_decode($response->getContent(), true);
-        //dd($content);
-        $this->assertEquals('2016-03-01 13:30:05', $content['notBefore']);
-
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the past is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
-        $content = json_decode($response->getContent(), true);
-        //dd($content);
-
-        $this->checkitemKeysExist($content);
-
-
-        $expectedNextTime = Carbon::now();
-
-        //Make the expected seconds right for the test
-        if ($expectedNextTime->second < 5) {
-            $expectedNextTime->second = 5;
-        }
-        else {
-            $expectedNextTime->minute++;
-            $expectedNextTime->second = 5;
-        }
-
-        $this->assertEquals($expectedNextTime, $content['notBefore']);
-
-        $this->assertEquals(200, $response->getStatusCode());
-
-        DB::rollBack();
-    }
 }
