affe68b1a4bb7195ccbb3ed723b38480c6260f98
Jenny Swift <cheezyspaghetti@gmail.com>
2016-04-02 00:12:56 +1100

    Make login page better for small screens

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -3,7 +3,7 @@
 <html lang="en" class="">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>Lists App</title>
     {{--<link rel="stylesheet" href="../tools/bootstrap.min.css">--}}
     {{--Bootstrap is the only one I need from plugins.css--}}
