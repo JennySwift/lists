0aa0d1dc73eb3af03dfdf40e6da971c9fdc496e5
Jenny Swift <cheezyspaghetti@gmail.com>
2016-04-02 00:24:24 +1100

    Don't show urgent items section if there are no urgent items

--- resources/views/pages/items/urgent-items-component.blade.php
+++ resources/views/pages/items/urgent-items-component.blade.php
@@ -1,20 +1,23 @@
 <script id="urgent-items-template" type="x-template">
 
     <div id="urgent-items">
-        
-        <h5>Urgent</h5>
 
-        <item
-                v-for="item in items | order"
-                :show-loading.sync="showLoading"
-                :show-item-popup.sync="showItemPopup"
-                :item="item"
-                :categories="categories"
-                :delete-item="deleteItem"
-                :selected-item.sync="selectedItem"
-                class="item-with-children"
-        >
-        </item>
+        <div v-if="items.length > 0">
+            <h5>Urgent</h5>
+
+            <item
+                    v-for="item in items | order"
+                    :show-loading.sync="showLoading"
+                    :show-item-popup.sync="showItemPopup"
+                    :item="item"
+                    :categories="categories"
+                    :delete-item="deleteItem"
+                    :selected-item.sync="selectedItem"
+                    class="item-with-children"
+            >
+            </item>
+        </div>
+
     </div>
 
 </script>
