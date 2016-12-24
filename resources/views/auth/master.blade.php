53d36e5b7abb0003f8be8ff04010862ddc445b50
Jenny Swift <cheezyspaghetti@gmail.com>
2016-12-24 13:59:25 +1100

    Fix login page

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -8,15 +8,9 @@
     {{--<link rel="stylesheet" href="../tools/bootstrap.min.css">--}}
     {{--Bootstrap is the only one I need from plugins.css--}}
     @include('templates.shared.head-links')
-    <style>
-        #navbar {display: flex;}
-        body footer {display: flex;}
-        body .main {display: block;}
-    </style>
 </head>
 
 <body>
-    @include('templates.shared.navbar-component')
     <div class="main">
         @section('content')
         @show
