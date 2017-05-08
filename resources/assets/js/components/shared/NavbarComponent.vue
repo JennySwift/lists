9fe8865d82909177d4fee0fc16e121fd1ee09a46
Jenny Swift <cheezyspaghetti@gmail.com>
2017-05-08 19:00:16 +1000

    Get navbar dropdowns working again

--- resources/assets/js/components/shared/NavbarComponent.vue
+++ resources/assets/js/components/shared/NavbarComponent.vue
@@ -1,58 +1,58 @@
 <template>
-    <ul id="navbar" style="z-index:1000">
 
-        <li id="menu-dropdown" class="dropdown">
-            <a href="#" class="dropdown-toggle fa fa-bars" data-toggle="dropdown"><span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                <li>
-                    <router-link to="/categories">Categories</router-link>
-                </li>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
 
-                <!--<li>-->
-                    <!--<a v-link="{ path: '/trash/'  }"><i class="fa fa-trash"></i></a>-->
-                <!--</li>-->
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-1">
+                <ul class="nav navbar-nav">
+                    <li v-bind:class="{'active': path === '/items' }">
+                        <router-link to="/items">Lists</router-link>
+                    </li>
 
-                <!--<li>-->
-                    <!--<a-->
-                        <!--v-on:click="undoDeleteItem()"-->
-                        <!--href="#">-->
-                        <!--undo delete item-->
-                    <!--</a>-->
-                <!--</li>-->
+                    <li>
+                        <a v-on:click="toggleFavouriteItems()" class="fa fa-star-o"></a>
+                    </li>
 
-                <li><a href="/auth/logout">Logout</a></li>
-            </ul>
-        </li>
+                    <li>
+                        <a v-on:click="toggleFilter()"><i class="fa fa-search"></i></a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <router-link to="/categories">Categories</router-link>
+                            </li>
 
-        <li>
-            <router-link to="/items">Lists</router-link>
-        </li>
+                            <li><a href="/auth/logout">Logout</a></li>
+                        </ul>
+                    </li>
 
-        <li>
-            <a v-on:click="toggleFavouriteItems()" class="fa fa-star-o"></a>
-        </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <router-link to="/feedback">Submit Feedback</router-link>
+                            </li>
 
-        <li>
-            <a v-on:click="toggleFilter()"><i class="fa fa-search"></i></a>
-        </li>
-
-        <li id="menu-dropdown" class="dropdown">
-            <a href="#" data-toggle="dropdown">
-                Help
-                <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu" role="menu">
-                <li>
-                    <router-link to="/feedback">Submit Feedback</router-link>
-                </li>
-
-                <li>
-                    <router-link to="/help">Help</router-link>
-                </li>
-            </ul>
-        </li>
-
-    </ul>
+                            <li>
+                                <router-link to="/help">Help</router-link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
 </template>
 
@@ -64,6 +64,11 @@
             return {
 
             };
+        },
+        computed: {
+            path: function () {
+              return this.$route.path;
+            }
         },
         components: {},
         methods: {
