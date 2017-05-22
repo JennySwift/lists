90381dcf5ca488be1ac29b134a4c608e9d05f231
Jenny Swift <cheezyspaghetti@gmail.com>
2017-05-22 11:41:33 +1000

    Change loading symbol

--- resources/assets/js/components/shared/LoadingComponent.vue
+++ resources/assets/js/components/shared/LoadingComponent.vue
@@ -1,6 +1,12 @@
 <template>
-    <div v-show="loading" id="loading">
-        <i class="fa fa-spinner fa-pulse"></i>
+    <!--<div v-show="loading" id="loading">-->
+        <!--<i class="fa fa-spinner fa-pulse"></i>-->
+    <!--</div>-->
+
+    <div v-show="loading" id="loading" class="sk-three-bounce">
+        <div class="sk-child sk-bounce1"></div>
+        <div class="sk-child sk-bounce2"></div>
+        <div class="sk-child sk-bounce3"></div>
     </div>
 </template>
 
