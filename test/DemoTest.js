e2fb7315dc0f8c9d0ead1f4467851d458ffff34a
Jenny Swift <cheezyspaghetti@gmail.com>
2017-06-03 00:00:51 +1000

    Try setting up testing with ava

---
+++ test/DemoTest.js
@@ -0,0 +1,7 @@
+var Vue = require('vue');
+var test = require('ava');
+var vm = new Vue(require('../resources/assets/js/components/ItemPopupComponent.vue'));
+
+test('that it renders a notification', function (t) {
+    
+});
