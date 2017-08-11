7e17fe5a5708867e211b7a6c2f0fba9f8a5e6ce3
Jenny Swift <cheezyspaghetti@gmail.com>
2017-08-11 20:21:20 +1000

    Try to get frontend testing working again

--- test/specs/DemoTest.js
+++
@@ -1,7 +0,0 @@
-var Vue = require('vue');
-var test = require('ava');
-var vm = new Vue(require('../../resources/assets/js/components/ItemPopupComponent.vue'));
-
-test('that it renders a notification', function (t) {
-    
-});
