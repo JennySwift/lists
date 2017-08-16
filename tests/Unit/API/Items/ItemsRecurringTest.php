007f5b1865b1f4134328bd662b69b50096d0bdee
Jenny Swift <cheezyspaghetti@gmail.com>
2017-08-16 11:22:30 +1000

    Refactor tests

--- tests/Unit/API/Items/ItemsRecurringTest.php
+++ tests/Unit/API/Items/ItemsRecurringTest.php
@@ -23,9 +23,7 @@ class ItemsRecurringTest extends TestCase
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
         $content = $this->getContent($response);
 
         $this->checkItemKeysExist($content);
@@ -47,18 +45,13 @@ class ItemsRecurringTest extends TestCase
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        //Make the recurring frequency 5
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'recurring_frequency' => 5
-        ]);
+        $response = $this->updateItem($item, ['recurring_frequency' => 5]);
         $content = $this->getContent($response);
-        //dd($content);
+
         $this->assertEquals(5, $content['recurringFrequency']);
 
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
+
         $content = $this->getContent($response);
         //dd($content);
 
@@ -81,22 +74,20 @@ class ItemsRecurringTest extends TestCase
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        //Make the recurring frequency 5 and the recurring frequency 'month' and the not before date way in the future (so it's fixed and testable rather than dynamic)
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
+        $response = $this->updateItem($item, [
+            //Make this way in the future (so it's fixed and testable rather than dynamic)
             'not_before' => '2050-01-10 15:30:00',
             'recurring_unit' => 'month',
             'recurring_frequency' => 5
         ]);
+
         $content = $this->getContent($response);
         //dd($content);
         $this->assertEquals('2050-01-10 15:30:00', $content['notBefore']);
         $this->assertEquals('month', $content['recurringUnit']);
         $this->assertEquals(5, $content['recurringFrequency']);
 
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
         $content = $this->getContent($response);
         //dd($content);
 
@@ -117,22 +108,20 @@ class ItemsRecurringTest extends TestCase
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        //Make the recurring frequency 2 and the recurring frequency 'year' and the not before date in the past
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
+        $response = $this->updateItem($item, [
+            //Make the not-before date in the past
             'not_before' => '2016-01-10 15:30:00',
             'recurring_unit' => 'year',
             'recurring_frequency' => 2
         ]);
+
         $content = $this->getContent($response);
-        //dd($content);
+
         $this->assertEquals('2016-01-10 15:30:00', $content['notBefore']);
         $this->assertEquals('year', $content['recurringUnit']);
         $this->assertEquals(2, $content['recurringFrequency']);
 
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
         $content = $this->getContent($response);
         //dd($content);
 
@@ -140,35 +129,32 @@ class ItemsRecurringTest extends TestCase
 
         $this->assertEquals('2018-01-10 15:30:00', $content['notBefore']);
 
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertResponseOk($response);
     }
 
     /**
      * @test
      * @return void
      */
-    public function it_can_calculate_the_next_time_for_a_recurring_item_that_has_a_not_before_time_three_years_ago_and_a_recurring_frequency_of_2_and_a_recurring_unit_of_years()
+    public function it_can_calculate_the_next_time_for_a_recurring_item_that_has_a_not_before_time_years_ago_and_a_recurring_frequency_of_2_and_a_recurring_unit_of_years()
     {
         $this->logInUser();
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        //Make the recurring frequency 2 and the recurring frequency 'year' and the not before date in the past
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
+        $response = $this->updateItem($item, [
             'not_before' => '2013-01-10 15:30:00',
             'recurring_unit' => 'year',
             'recurring_frequency' => 2
         ]);
+
         $content = $this->getContent($response);
-        //dd($content);
+
         $this->assertEquals('2013-01-10 15:30:00', $content['notBefore']);
         $this->assertEquals('year', $content['recurringUnit']);
         $this->assertEquals(2, $content['recurringFrequency']);
 
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the future is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
         $content = $this->getContent($response);
         //dd($content);
 
@@ -193,19 +179,13 @@ class ItemsRecurringTest extends TestCase
         $item = $this->getFirstRecurringItem();
         $this->checkItemIsAsExpected($item);
 
-        //Make the not before time in the past
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'not_before' => '2016-03-01 13:30:05'
-        ]);
+        $response = $this->updateItem($item, ['not_before' => '2016-03-01 13:30:05']);
         $content = $this->getContent($response);
-        //dd($content);
+
         $this->assertEquals('2016-03-01 13:30:05', $content['notBefore']);
 
         //Todo: it's this line that's making it really slow to run
-        //Check it calculates the next time correctly, for when the instance of the recurring item in the past is completed
-        $response = $this->call('PUT', '/api/items/'.$item->id, [
-            'updatingNextTimeForRecurringItem' => true
-        ]);
+        $response = $this->rescheduleItem($item);
         $content = $this->getContent($response);
         //dd($content);
 
@@ -248,6 +228,31 @@ class ItemsRecurringTest extends TestCase
         $this->assertEquals('minute', $item->recurring_unit);
         $this->assertEquals(1, $item->recurring_frequency);
         $this->assertEquals(Carbon::tomorrow()->format('Y-m-d H:i:s'), $item->not_before);
+    }
+
+    /**
+     *
+     * @param $item
+     * @param $newData
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    private function updateItem($item, $newData)
+    {
+        $response = $this->call('PUT', '/api/items/' . $item->id, $newData);
+
+        return $response;
+    }
+
+    /**
+     * For when the instance of the recurring item in the past or the future is completed
+     * @param $item
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    private function rescheduleItem($item)
+    {
+        $response = $this->updateItem($item, ['updatingNextTimeForRecurringItem' => true]);
+
+        return $response;
     }
 
 
