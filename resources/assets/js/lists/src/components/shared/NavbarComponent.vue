3c4b6120754c779b5b2612273f795c69c2b1bd88
Jenny Swift <cheezyspaghetti@gmail.com>
2018-02-18 21:30:28 +1100

    Make navbar better for small screens

--- resources/assets/js/lists/src/components/shared/NavbarComponent.vue
+++ resources/assets/js/lists/src/components/shared/NavbarComponent.vue
@@ -1,69 +1,38 @@
 <template>
 
-    <nav id="navbar" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <!--<ul class="nav pull-left">-->
-                    <!--<li><button id="new-item-btn" class="btn btn-default btn-sm">New Item</button></li>-->
+    <nav id="navbar">
+        <!--<div>-->
+            <!--<button type="button">-->
+                <!--<span class="sr-only">Toggle navigation</span>-->
+                <!--<span class="icon-bar"></span>-->
+                <!--<span class="icon-bar"></span>-->
+                <!--<span class="icon-bar"></span>-->
+            <!--</button>-->
+        <!--</div>-->
 
-                <!--</ul>-->
-
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar-1">
-                <ul class="nav navbar-nav">
+        <ul>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
+                <ul class="dropdown-menu">
                     <li>
-                        <router-link to="/items">Items</router-link>
-                    </li>
-
-                    <!--<li>-->
-                        <!--<a v-on:click="toggleFavouriteItems()">Favourites</a>-->
-                    <!--</li>-->
-
-                    <li>
-                        <a v-on:click="toggleFilter()">Filter/Search</a>
-                    </li>
-                </ul>
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <router-link to="/categories">Categories</router-link>
-                            </li>
-
-                            <li><a href="/logout">Logout</a></li>
-                        </ul>
+                        <router-link to="/categories">Categories</router-link>
                     </li>
 
                     <li>
                         <router-link to="/trash">Trash</router-link>
                     </li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <!--<li>-->
-                                <!--<router-link to="/feedback">Submit Feedback</router-link>-->
-                            <!--</li>-->
-
-                            <li>
-                                <router-link to="/help">Help</router-link>
-                            </li>
-                        </ul>
+                    <li>
+                        <router-link to="/help">Help</router-link>
                     </li>
+                    <li><a href="/logout">Logout</a></li>
                 </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
+            </li>
+        </ul>
+
+        <router-link to="/items">Items</router-link>
+
+        <div v-on:click="toggleFilter()">Filter/Search</div>
+
     </nav>
 
 </template>
