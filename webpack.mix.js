5b9e7ee60f77644cabe7a6d4bc4dd40e240d4473
Jenny Swift <cheezyspaghetti@gmail.com>
2018-06-01 15:11:04 +1000

    Use versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,5 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/js/login.js', 'public/js')
     .sourceMaps()
-    // .version()
+    .version()
    .sass('resources/assets/sass/app.scss', 'public/css');
