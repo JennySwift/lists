be6e9b6bb8104d365db108f34a3b5c238cec9842
Jenny Swift <cheezyspaghetti@gmail.com>
2018-07-14 15:51:39 +1000

    Set up database backups

--- config/app.php
+++ config/app.php
@@ -150,7 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
-//        Spatie\Backup\BackupServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
 //        Laravel\Passport\PassportServiceProvider::class,
 
         /*
